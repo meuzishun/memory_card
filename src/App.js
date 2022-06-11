11be798fbde8aa89b52d03a86a3107a64c64d7e8
Andrew Smith <meuzishun@gmail.com>
2022-06-11 16:13:29 -0400

    Create function for selecting 10 cards

--- src/App.js
+++ src/App.js
@@ -7,14 +7,44 @@ import Footer from './components/Footer';
 import buildCards from './utilities/buildCards';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      randomChoices: [],
+    };
+
+    this.chooseRandomCards = this.chooseRandomCards.bind(this);
+  }
+
+  chooseRandomCards() {
+    const allCards = [...buildCards];
+    const randomChoices = [];
+    for (let i = 0; i < 10; i++) {
+      randomChoices.push(
+        ...allCards.splice(Math.floor(Math.random() * allCards.length), 1)
+      );
+    }
+    console.log('original', buildCards);
+    console.log('copied', allCards);
+    this.setState(
+      {
+        randomChoices,
+      },
+      () => {
+        console.log(this.state.randomChoices);
+      }
+    );
+  }
+
   render() {
-    console.table(buildCards);
     return (
       <div className='App'>
         <Header />
         <Scoreboard />
         <Gameboard />
         <Footer />
+        <button onClick={this.chooseRandomCards}>Log some cards</button>
       </div>
     );
   }
