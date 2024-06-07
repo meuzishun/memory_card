f627015dc0ff4c70be03866c1dcc5b63e66846d4
Andrew Smith <meuzishun@gmail.com>
2024-06-07 15:52:17 -0400

    Delete commented-out code from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import React, { Component } from 'react';
 import React, { useState } from 'react';
 import styles from './styles/App.module.css';
 import Header from './components/Header';
@@ -6,84 +5,6 @@ import Scoreboard from './components/Scoreboard';
 import Gameboard from './components/Gameboard';
 import Footer from './components/Footer';
 import buildCards from './utilities/buildCards';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       randomChoices: [],
-//       previouslyChosen: [],
-//       highScore: 0,
-//     };
-
-//     this.chooseRandomCards = this.chooseRandomCards.bind(this);
-//     this.emptyChosenCards = this.emptyChosenCards.bind(this);
-//     this.handleCardClick = this.handleCardClick.bind(this);
-//   }
-
-//   chooseRandomCards() {
-//     const allCards = [...buildCards];
-//     const randomChoices = [];
-
-//     for (let i = 0; i < 10; i++) {
-//       randomChoices.push(
-//         ...allCards.splice(Math.floor(Math.random() * allCards.length), 1)
-//       );
-//     }
-
-//     this.setState({
-//       randomChoices,
-//     });
-//   }
-
-//   emptyChosenCards() {
-//     this.setState({
-//       randomChoices: [],
-//     });
-//   }
-
-//   handleCardClick(e) {
-//     const card = e.currentTarget;
-//     const { rank, suit } = card.dataset;
-//     const alreadyChosen = this.state.previouslyChosen.some((choice) => {
-//       return choice.rank === rank && choice.suit === suit;
-//     });
-
-//     if (!alreadyChosen) {
-//       this.setState({
-//         previouslyChosen: [...this.state.previouslyChosen, { rank, suit }],
-//       });
-//       this.chooseRandomCards();
-//     } else {
-//       this.setState({
-//         previouslyChosen: [],
-//         highScore:
-//           this.state.previouslyChosen.length > this.state.highScore
-//             ? this.state.previouslyChosen.length
-//             : this.state.highScore,
-//       });
-//       this.emptyChosenCards();
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div className={styles.App}>
-//         <Header startGame={this.chooseRandomCards} />
-//         <Scoreboard
-//           score={this.state.previouslyChosen.length}
-//           high={this.state.highScore}
-//         />
-//         <Gameboard
-//           choices={this.state.randomChoices}
-//           cb={this.handleCardClick}
-//         />
-//         <Footer />
-//       </div>
-//     );
-//   }
-// }
 
 function App() {
   const [randomChoices, setRandomChoices] = useState([]);
